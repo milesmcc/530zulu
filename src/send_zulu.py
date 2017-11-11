03c60ea7facbc283426a7746dd7c628011524b63
Miles McCain <milesmcc@users.noreply.github.com>
2017-11-11 00:01:39 -0500

    add better debug message

--- src/send_zulu.py
+++ src/send_zulu.py
@@ -1,6 +1,10 @@
 import craft
 import messenger
 
+import datetime
+
+print("Sending 530ZULU... current time: " + datetime.datetime.utcnow().isoformat())
+
 # craft newsletter
 data = craft.craft_newsletter()
 
@@ -12,3 +16,4 @@ text = craft.craft_text(data)
 
 # send email
 messenger.send_mail(name=data["name"], subject=data["name"], preview="Everything you need to know to stay ahead.", html=html, text=text)
+print("--------------- end of execution ---------------")
